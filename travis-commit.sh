3e9df9178536cc4b067b2a3a5ac159c71422223e
ujue <4507425+UjuE@users.noreply.github.com>
2019-10-26 21:20:44 +0100

    More experimentation

---
+++ travis-commit.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_version_files() {
+  git checkout master
+  git add -f version.txt next-version.txt
+  # with "[skip ci]" to avoid a build loop
+  git commit -m "Updating version files (Version $TRAVIS_TAG) (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  git push origin master --quiet
+}
+
+setup_git
+
+commit_version_files
+
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "Updating version files to github"
+  upload_files
+else
+  echo "No changes in version files."
+fi
